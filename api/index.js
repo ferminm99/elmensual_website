a93a20ee89b6a1b8be39da16b673cd24e516c34d
Ferminm99 <ferminmorenoz99@gmail.com>
2024-10-30 21:13:07 -0300

    El deploy en render anda bien.

--- api/index.js
+++ api/index.js
@@ -26,10 +26,6 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.get("/api/test", (req, res) => {
-  console.log("API TEST LOADING ", req);
-  res.send("API is working");
-});
 
 // app.use("/api/checkout", stripeRoute);
 
