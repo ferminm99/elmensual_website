cdb94f9e6c34b1a871869646ddf9d060c515b728
Ferminm99 <ferminmorenoz99@gmail.com>
2024-10-30 23:38:41 -0300

    seguimos... 4

--- client/src/requestMethods.ts
+++ client/src/requestMethods.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // Agrega el console.log aquí para verificar que `NEXT_PUBLIC_API_URL` esté definido
-console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 const user = JSON.parse(localStorage.getItem("persist:root") || "{}")?.user;
 const currentUser = user && JSON.parse(user)?.currentUser;
 const TOKEN: string | undefined = currentUser?.accessToken;
