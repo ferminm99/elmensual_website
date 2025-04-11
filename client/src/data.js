8ca452f2cf865dc2c3c837a8489da1a63b75d51c
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-10 23:06:41 -0300

    fix alforzada

--- client/src/data.js
+++ client/src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://elmensual.com.ar/images/alforzada1.png",
+    img: "https://elmensual.com.ar/images/alforzada3.png",
     title: "BOMBACHAS DE GRAFA",
     desc: "EXPLORA NUESTRAS OPCIONES DE BOMBACHA DE CAMPO DE GRAFA PESADA O LIVIANA",
     bg: "f5fafd",
@@ -9,7 +9,7 @@ export const sliderItems = [
   },
   {
     id: 2,
-    img: "https://elmensual.com.ar/images/alforzada1.png",
+    img: "https://elmensual.com.ar/images/alforzada3.png",
     title: "BOMBACHAS DE POPLIN/GRAFIL",
     desc: "LAS BOMBACHAS DE CAMPO DE POPLIN SON LA COMBINACION IDEAL DE ALGODON Y POLIESTER PARA NO SUFRIR EL CALOR",
     bg: "f5fafd",
@@ -17,7 +17,7 @@ export const sliderItems = [
   },
   {
     id: 3,
-    img: "https://elmensual.com.ar/images/alforzada1.png",
+    img: "https://elmensual.com.ar/images/alforzada3.png",
     title: "BOMBACHAS DE CORDEROY",
     desc: "NUESTRAS BOMBACHAS DE CORDEROY SON DE LA MEJOR CALIDAD Y MAS TRABAJADAS PARA QUE PASES TU INVIERNO COMODAMENTE",
     bg: "f5fafd",
@@ -28,7 +28,7 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://elmensual.com.ar/images/alforzada1.png",
+    img: "https://elmensual.com.ar/images/alforzada3.png",
     title: "ROPA DE CAMPO DE HOMBRE",
     cat: "hombre",
   },
