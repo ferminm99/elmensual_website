c2224730fd22ebc05f730749fc5a6e5732e0b72b
Ferminm99 <ferminmorenoz99@gmail.com>
2025-11-15 15:39:19 -0300

    agrego al cors lo que corresponde

--- api/index.js
+++ api/index.js
@@ -25,6 +25,7 @@ const ALLOWED_ORIGINS = [
   "http://localhost:5000",
   "http://127.0.0.1:3000",
   "https://elmensual-website.onrender.com",
+  "https://elmensualwebsite-production.up.railway.app",
   // agrega aquí otros frontends que uses (Vercel, etc.)
 ];
 
