011799231ce740e2ee721cadd940d20d4d6fb9bf
Ferminm99 <ferminmorenoz99@gmail.com>
2025-11-15 15:47:09 -0300

    /api para la base url

--- admin/src/requestMethods.ts
+++ admin/src/requestMethods.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "./redux/store";
 import { logout } from "./redux/userRedux";
 
-const BASE_URL = "https://elmensualwebsite-production.up.railway.app";
+const BASE_URL = "https://elmensualwebsite-production.up.railway.app/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
